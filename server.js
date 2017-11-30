190f4edb187282a59b4b1b54c0b61989aa154b7a
mikewhite1996 <33696861+mikewhite1996@users.noreply.github.com>
2017-11-30 12:30:13 -0330

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
-/**
- * http://usejsdoc.org/
+* http://usejsdoc.org/
  */
-var babel = require('babel-register');
+//var babel = require('babel-register');
 var fs = require('fs');
-//var reach = require('./public/javascripts/SCourses2.js');
-//var data = fs.readFileSync("student-2.json");
-//var words = JSON.parse(data);
-//console.log(words);
 var express = require("express");
 var app     = express();
 var path    = require("path");
@@ -48,59 +43,11 @@ request.post(
 );
 
 var qs = require('querystring');
-/*function data(request, response) {
-    if (request.method == 'POST') {
-        var body = '';
 
-        request.on('data', function (data) {
-            body += data;
-
-            // Too much POST data, kill the connection!
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1e6)
-                request.connection.destroy();
-        });
-
-        request.on('end', function () {
-            var post = qs.parse(body);
-            // use post['blah'], etc.
-            console.log(post.courseNum);
-        });
-    }
-}*/
 
 var http = require('http');
 
-http.createServer((request, response) => {
-  var { headers, method, url } = request;
-  var body = [];
-  request.on('error', (err) => {
-    console.error(err);
-  }).on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
-    // BEGINNING OF NEW STUFF
 
-    response.on('error', (err) => {
-      console.error(err);
-    });
-
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.writeHead(200, {'Content-Type': 'application/json'})
-
-    const responseBody = { headers, method, url, body };
-
-    response.write(JSON.stringify(responseBody));
-    response.end();
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.end(JSON.stringify(responseBody))
-
-    // END OF NEW STUFF
-  });
-}).listen(3331);
 
 console.log(http);
 
@@ -115,13 +62,22 @@ app.use(bodyParser.urlencoded({
  */
 app.use(bodyParser.json());
 
-app.post("/getdata", function (req, res) {
-    console.log(req.body.user.name);
+app.post("/", function (req, res) {
+	var object = req.body.user.name;
+	var name = JSON.stringify(object);
+	console.log(name);
+	console.log(object);
+	fs.writeFile('CONTENT.json', object, 'utf8');
+	
+	
+	
+	
 });
 
 app.listen(3331);
 
 console.log("Running at Port 3331");
+
 
 
 
